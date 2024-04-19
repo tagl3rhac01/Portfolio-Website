8321d2fc00adda2250f49d3623fd7813222d10f6
tagl3rhac01 <tagl3rhac01@gmail.com>
2024-04-19 21:07:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Developer portfolio that's a 2D game
+
+Inspired by the many creative 3D developer portfolios, I create one that's 2D instead.It works on both desktop
+and mobile.
+
+![A screenshot of the project](./ssofwebsite.png)
+
+Live Demo : "TODO"
+
+
+
+# How to run
+
+Note: You need `Node.js` and `npm` installed on your machine.
+
+`npm install` then `npm run dev`
+
+# How to build
+
+`npm run build` and a dist folder should be created.
+
+# How to preview the build
+
+`npm run preview`
+
+# How to host?
---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/tagl3rhac01" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@harkirat1" target="_blank">Harkirat Singh</a>, <a href="https://www.youtube.com/@LoveBabbar" target="_blank">Love Babbar</a>,
+  <a href="https://www.youtube.com/@ezsnippat" target="_blank">Neeraj Walia</a> (sometimes) and <a href="https://www.youtube.com/@TechLead" target="_blank">Techlead</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/1LIx7st2I2V7qmCd-IModGmZkqNVwRDj1RFjTnG--kTI/edit?usp=sharing" target="_blank">Check it out?</a>
+  Contact me at work.jatinchandok10@gmail.com AND YES, PLEASE HIRE ME!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. There is even one in Japanese (I also speak Japanese btw).
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Jatin Chandok's portfolio, just close the tab.`,
+};
